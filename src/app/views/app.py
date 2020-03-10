51aca0518499b0e549c3ca7cea0edba3a7653592
Warsa <warsa@vpn-10-52-109-133.vpn-s.ntnu.no>
2020-03-10 18:31:54 +0100

    Satte web.config.debuf til false sånn at feilmeldinger ikke vises

--- src/app/views/app.py
+++ src/app/views/app.py
@@ -41,7 +41,7 @@ render = web.template.render('templates/')
 
 # Set session timeout
 web.config.session_parameters['timeout'] = 15000000
-
+web.config.debug=False
 # Workaround to use sessions with reloader (debugger) http://webpy.org/cookbook/session_with_reloader
 if web.config.get('_session') is None:
     session = web.session.Session(app, web.session.DiskStore("sessions"), initializer={"username": None})
